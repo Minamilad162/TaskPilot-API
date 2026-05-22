06d81538356ab4e89e82e4b731a78a7c57c0ad2c
MinaHrcom <Mina.M@hrcom.io>
2026-05-22 22:05:10 +0300

    Final Fixes and verification app

--- src/ProjectTaskManagement.Application/DependencyInjection.cs
+++ src/ProjectTaskManagement.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using ProjectTaskManagement.Application.Projects.Services;
+using ProjectTaskManagement.Application.Tasks.Services;
 
 namespace ProjectTaskManagement.Application;
 
@@ -8,6 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<IProjectTaskService, ProjectTaskService>();
 
         return services;
     }
